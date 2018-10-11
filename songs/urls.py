9655c4e9b58cdadfac9130cf1deaa3a59be4cf5d
Efren Mercado Jr <efren.mercado.jr@dynameyes.com>
2018-10-11 20:54:50 +0800

    Add top songs endpoint

--- songs/urls.py
+++ songs/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 
 from rest_framework import routers
 
-from .views import SongViewSet
+from .views import SongViewSet, TopSongsView
 
 router = routers.SimpleRouter()
-router.register(r'songs', SongViewSet)
-urlpatterns = router.urls
+router.register(r'', SongViewSet)
+
+urlpatterns = [
+    path(r'songs/top/', TopSongsView.as_view()),
+    path(r'songs/', include((router.urls))),
+]
--- songs/views.py
+++ songs/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework import generics
 
 from .serializers import SongSerializer
 from .models import Song
@@ -8,3 +9,11 @@ from .models import Song
 class SongViewSet(viewsets.ModelViewSet):
     serializer_class = SongSerializer
     queryset = Song.objects.all()
+
+
+class TopSongsView(generics.ListAPIView):
+    serializer_class = SongSerializer
+    queryset = Song.objects.all()
+
+    def get_queryset(self):
+        return Song.objects.order_by('views')[:3]
