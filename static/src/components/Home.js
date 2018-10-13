651480c016b5e58022dc8c277df5174040339607
Efren Mercado Jr <efren.mercado.jr@dynameyes.com>
2018-10-13 14:21:52 +0800

    Fix top songs

--- static/src/components/Home.js
+++ static/src/components/Home.js
@@ -15,61 +15,7 @@ class Home extends Component {
 
   getSongs() {
     axios.get("http://127.0.0.1:8000/api/songs/top/").then(results => {
-      //var topsongs = this.state.topSongs;
-      //this.setState({ topSongs: topsongs });
-      //console.log(this.state);
-
-      //sort data by views
-      var views = [];
-      var j;
-      for (j = 0; j < results.data.length; j++) {
-        views.push(results.data[j].views);
-      }
-      console.log(views);
-      views.sort(function(a, b) {
-        return b - a;
-      });
-      console.log(views);
-
-      //get top 3
-      var top = [];
-      var m1, m2, m3;
-      for (m1 = 0; m1 < results.data.length; m1++) {
-        if (results.data[m1].views === views[0]) {
-          top.push(results.data[m1]);
-        }
-      }
-      for (m2 = 0; m2 < results.data.length; m2++) {
-        if (results.data[m2].views === views[1]) {
-          top.push(results.data[m2]);
-        }
-      }
-      for (m3 = 0; m3 < results.data.length; m3++) {
-        if (results.data[m3].views === views[2]) {
-          top.push(results.data[m3]);
-        }
-      }
-      this.setState({ topSongs: top });
-      //console.log(this.state);
-      // for(i = 0; i < 3; i++){
-      //Math.max(results.data.views)
-      //res.push(results.data[i])
-      // }
-      //var i;
-      //var res = [];
-      //for (i = 0; i < length; i++) {
-      //if (results.data[i].title.includes(searchFor)) {
-      //if (results.data[i].title.toLowerCase() === searchFor.toLowerCase()) {
-      //console.log("HELLO");
-      //res.push(results.data[i]);
-      //this.setState({ songs: results.data[i] });
-      // }
-      //}
-      //this.setState({ songs: res });
-      //console.log(res);
-      //console.log(results);
-      //console.log(length);
-      //console.log(results.data);
+      this.setState({ topSongs: results.data })
     });
   }
 
